f41fbeb7e9cba387c21e8f2d0b08f7b0346a4e3a
Vladimir-Beliy <vovaazick@gmail.com>
2020-04-24 22:30:34 +0300

    Updates

---
+++ precache-manifest.0445e64073c326d6b322f3efa8023e52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e5c3a7ffcdfcb22e1016c05d127a24",
+    "url": "./index.html"
+  },
+  {
+    "revision": "495b0cdd58305d95bf87",
+    "url": "./static/css/main.bf95cc9e.chunk.css"
+  },
+  {
+    "revision": "04a505b1cc6507443b51",
+    "url": "./static/js/2.5d064338.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5d064338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495b0cdd58305d95bf87",
+    "url": "./static/js/main.b09f92a2.chunk.js"
+  },
+  {
+    "revision": "c7101ef3e7606a672d39",
+    "url": "./static/js/runtime-main.0c400398.js"
+  }
+]);
